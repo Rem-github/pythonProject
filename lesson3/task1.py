d8aa25b731b5fd6f07d3d39188b26d985531437b
Roman <radrr@mail.ru>
2021-01-31 10:13:53 +0300

    Добавлена функция input_float

--- lesson3/task1.py
+++ lesson3/task1.py
@@ -1,12 +1,16 @@
 
 # Эта программа принимает в функцию два числа и делит их
+# Добавлена функция которая ждет от пользователя только число типа float
 
-def make_float(x):
-        try:
-            x = float(x)
-            return x
-        except ValueError:
-            print("Необходимо вводить числа")
+def input_float():
+        x = ''
+        while('.' not in x):
+            x = input()
+            try:
+                float(x)
+            except:
+                x = ''
+        return float(x)
 
 def division_numbers(a, b):
     try:
@@ -14,10 +18,10 @@ def division_numbers(a, b):
     except ZeroDivisionError:
         print("На ноль делить нельзя")
 
-a = make_float(input("Введите число a: "))
-if a != None:
-    b = make_float(input("Введите число b: "))
-    if b != None:
-        c = division_numbers(a, b)
-        if c != None:
-            print(f'Полученное число: {c:.2f}')
+print('Введите первое число типа float:')
+a = input_float()
+print('Введите второе число типа float:')
+b = input_float()
+c = division_numbers(a, b)
+if c != None:
+    print(f'Полученное число: {c:.2f}')
